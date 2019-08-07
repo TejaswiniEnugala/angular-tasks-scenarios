b8172203dd14dbef5cf4a697e4924bdc76d8b1b1
TejaswiniEnugala <tejaswinienugala.in@gmail.com>
2019-08-07 12:54:17 +0530

    task1 completed

---
+++ src/app/scenario1/container1/container1.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-container1',
+  templateUrl: './container1.component.html',
+  styleUrls: ['./container1.component.css']
+})
+export class Container1Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+ myfunction(value)
+{
+  console.log(value)
+}
+}
